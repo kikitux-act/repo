b14a4ad8f3ae8009b3dd51e6bc3b15a22f6a5617
Michael Chew <michael.chew@actifio.com>
2018-10-14 20:42:35 +1100

    Create inst-actconnector.ps1

---
+++ windows/inst-actconnector.ps1
@@ -0,0 +1,44 @@
+## Perform a silent install Actifio connector
+##
+
+param (
+[string]$ActifioIP = ""
+)
+
+if ($ActifioIP -eq "") {
+  $ActifioIP  = Read-Host -Prompt 'Please enter the Actifio IP address '
+} 
+
+# Path for the temporary workdir
+$workdir = $env:TEMP
+
+# Download the installer
+
+$source = "http://$ActifioIP/connector-Win32-latestversion.exe"
+$destination = "$workdir\connector.exe"
+
+# Check if Invoke-Webrequest exists otherwise execute WebClient
+
+if (Get-Command 'Invoke-Webrequest')
+{
+     Invoke-WebRequest $source -OutFile $destination
+}
+else
+{
+    $WebClient = New-Object System.Net.WebClient
+    $webclient.DownloadFile($source, $destination)
+}
+
+# Start the installation
+
+# /type=compact is to not install the Filter Driver
+# Start-Process -FilePath "$workdir\connector.exe" -ArgumentList "/silent /type=compact "
+
+# /type=full is to install the Filter Driver
+Start-Process -FilePath "$workdir\connector.exe" -ArgumentList "/silent /type=full "
+
+# Wait XX Seconds for the installation to finish
+Start-Sleep -s 35
+
+# Remove the installer directory
+Remove-Item $workdir\connector.exe -Force
