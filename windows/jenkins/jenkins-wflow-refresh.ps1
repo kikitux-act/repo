bdd26576b6c65872338387bccbd201fd9b718f09
Michael Chew <michael.chew@actifio.com>
2018-10-15 08:54:16 +1100

    Update and rename jenkin-wflow-build.ps1 to jenkins-wflow-refresh.ps1

--- windows/jenkins/jenkins-wflow-refresh.ps1
+++ windows/jenkins/jenkins-wflow-refresh.ps1
@@ -11,23 +11,23 @@ $workflow_name = $Env:WorkflowList -replace "`n", ""
 $workflow_id = $(udsinfo lsworkflow | where-object {$_.name -eq $workflow_name}).id
 write-output "Kicking off workflow $workflow_name ( ID: $workflow_id ) `n"
 
-$rc = $(udstask chworkflow -disable false $workflow_id | out-null)    
-if ($rc.result -ne $workflow_id) { write-output "There is a problem with workflow $workflow_name - $rc.errormessage" ; exit 1 }
+$rc = $(udstask chworkflow -disable false $workflow_id)    
+if ($rc.result -ne $workflow_id) { write-output "There is a problem disabling the workflow $workflow_name - $rc.errormessage" ; exit 1 }
 
 udstask runworkflow $workflow_id | out-null   
 $rc = $(reportworkflows -s | where-object { $_.ID -eq  $workflow_id })
-if ($rc.status -ne "RUNNING") { write-output "Unable to trigger workflow - $rc.steps" ; exit 1}
+if ($rc.status -ne "RUNNING") { write-output "Unable to trigger workflow - $($rc.steps)" ; exit 1}
 
-$rc = $(udstask chworkflow -disable true $workflow_id | out-null)    
-if ($rc.result -ne $workflow_id) { write-output "There is a problem with workflow $workflow_name - $rc.errormessage" ; exit 1 }
+$rc = $(udstask chworkflow -disable true $workflow_id)    
+if ($rc.result -ne $workflow_id) { write-output "There is a problem enabling with workflow $workflow_name - $rc.errormessage" ; exit 1 }
 
-sleep -Seconds 45
+sleep -Seconds 30
 $srchost = $(reportworkflows | where {$_.WorkflowName -eq $workflow_name } ).SourceHostName
 $appname = $(reportworkflows | where {$_.WorkflowName -eq $workflow_name } ).SourceAppName
 $vdb = $(reportworkflows | where {$_.WorkflowName -eq $workflow_name } ).TargetApp
 $appid = $(reportworkflows | where {$_.WorkflowName -eq $workflow_name } ).SourceAppID
 
-$JobID = $(reportrunningjobs | where { $_.HostName -eq $srchost -And $_.AppName -eq $appname }).JobName
+$JobID = $(reportrunningjobs | where { $_.HostName -eq $srchost -And $_.AppName -eq $appname -And $_.JobClass -eq 'mount(AppAware)' }).JobName
 $JobStatus = $(reportrunningjobs | where { $_.JobName -eq $JobID }).status
 $PrevJobPct = "0"
 if ($JobStatus -eq 'running') {
@@ -46,7 +46,7 @@ if ($JobStatus -eq 'running') {
 sleep -Seconds 10
 
 $rc = $(reportworkflows -s | where-object { $_.ID -eq  $workflow_id })
-if ($rc.status -ne "SUCCESS") { write-output "Refresh job has failed - $rc.steps" ; exit 1}
+if ($rc.status -ne 'SUCCESS') { write-output "Refresh job has failed - $rc.steps" ; exit 1}
 
 $start = $(udsinfo lsjobhistory $JobID).startdate
 $duration = $(udsinfo lsjobhistory $JobID).duration
