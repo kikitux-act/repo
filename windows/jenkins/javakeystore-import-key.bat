a188abe42b266d40b8994ac837167f3ffeec907d
Michael Chew <michael.chew@actifio.com>
2018-10-15 08:46:33 +1100

    Create javakeystore-import-key.bat

---
+++ windows/jenkins/javakeystore-import-key.bat
@@ -0,0 +1,2 @@
+:: changeit
+"C:\Program Files (x86)\Jenkins\jre\bin\keytool.exe" -import -file "c:\data\melagm.cer" -keystore "C:\Program Files (x86)\Jenkins\jre\lib\security\cacerts" -alias melagm
