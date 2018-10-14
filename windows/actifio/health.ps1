29c64b8635882dc25d9faa52626aec773a28f2ff
Michael Chew <michael.chew@actifio.com>
2018-10-14 20:29:22 +1100

    Create health.ps1

---
+++ windows/actifio/health.ps1
@@ -0,0 +1,18 @@
+param(
+ [string] $ActUser = 'actifio01',
+ [string] $ActIP = "10.19.123.239")
+
+$pwfile = "C:\scripts\ps\$env:USERNAME-passwd.key"
+
+#################################
+# Connect to Actifio appliance  #
+#################################
+
+##  Save-ActPassword –filename "C:\scripts\password.key"
+"password" | ConvertTo-SecureString –AsPlainText –Force | ConvertFrom-SecureString | Out-File $pwfile
+
+connect-act -acthost $ActIP -actuser $ActUser -passwordfile $pwfile -ignorecerts
+
+reporthealth
+
+disconnect-act
