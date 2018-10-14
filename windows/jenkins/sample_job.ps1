2d02b9a0e4c7e274e4fe370262ef29a934991f59
Michael Chew <michael.chew@actifio.com>
2018-10-14 22:51:56 +1100

    Update sample_job.ps1

--- windows/jenkins/sample_job.ps1
+++ windows/jenkins/sample_job.ps1
@@ -1,3 +1,9 @@
 Write‐Output "The hostname of this machine is $env:ComputerName"
 Write‐Output "The PowerShell version details on this machine are as follows:"
 $PSVersionTable
+
+$env:username
+$env:psmodulepath
+$env:clientname
+
+$server = $env:computername  
