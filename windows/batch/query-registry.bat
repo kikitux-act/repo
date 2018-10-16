d53ca0f6b49b84a040ea4dc5d6c79aa5fb7788e3
Michael Chew <michael.chew@actifio.com>
2018-10-16 21:54:10 +1100

    Update query-registry.bat

--- windows/batch/query-registry.bat
+++ windows/batch/query-registry.bat
@@ -14,3 +14,14 @@ reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 reg query "HKCU\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer\Run\COM Service"
 
 reg query "HKEY_USERS\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+
+
+@echo off
+echo Inspecting the values of Run and RunOnce 
+
+reg query HKLM\Software\Microsoft\Windows\CurrentVersion\Run
+reg query HKLM\Software\Microsoft\Windows\CurrentVersion\RunOnce
+reg query HKLM\Software\Microsoft\Windows\CurrentVersion\RunonceEx
+
+reg query HKCU\Software\Microsoft\Windows\CurrentVersion\Run
+reg query HKCU\Software\Microsoft\Windows\CurrentVersion\RunOnce
